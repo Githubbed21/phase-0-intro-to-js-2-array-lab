f0126feabd28f54929392d51c1cf96c23e5b0736
dean <diazdean21@gmail.com>
2022-09-20 17:29:14 -0500

    commit tree 3df12dd99180f7e586e878a2df63efe3cfcbb74e

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Write your solution here!
 
-const cats = ["Milo", "Otis", "Garfield"]
+const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    console.log("Name:", name);
-    cats.push(name)
+  console.log("Name:", name);
+  cats.push(name);
 }
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
+  cats.unshift(name);
 }
 function destructivelyRemoveLastCat(name) {
-    cats.pop(name)
+  cats.pop(name);
 }
 function destructivelyRemoveFirstCat() {
-    cats.shift();
+  cats.shift();
 }
 function appendCat(name) {
-    return [...cats, name]
+  return [...cats, name];
 }
 function prependCat(name) {
-    return[...cats, name];
+  return [...cats, name];
 }
 function prependCat(name) {
-    return [name, ...cats]
+  return [name, ...cats];
 }
 function removeLastCat(name) {
-    return cats.slice(0, -1)
+  return cats.slice(0, -1);
 }
 function removeFirstCat(name) {
-    return cats.slice(1)
+  return cats.slice(1);
 }
